460ae1b9988fba159536be279abe17d755d154ba
Pavel Svejda <pavlis.1@seznam.cz>
2023-02-09 23:27:28 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    /**
+     * Komentar, z ktereho se da generovat dokumentace trid, ci metod
+     * @param args
+     */
+    public static void main(String[] args) {
+        // Jednoradkovy komentar.. ciste jen tak pro formu :)
+        System.out.println("Hello World!");
+        /*
+         * Toto je priklad viceradkoveho
+         * komentare...
+         */
+    }
+}
